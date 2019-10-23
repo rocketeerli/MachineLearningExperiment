91f07e023f0eb0895efc4fd12afdfb338780c0e2
rocketeerLi <31384630+rocketeerli@users.noreply.github.com>
2019-10-23 23:10:50 +0800

    finish newton

--- lab2/main.py
+++ lab2/main.py
@@ -10,17 +10,45 @@ def sigmoid(z):
 # 将函数向量化
 sigmoid_vec = np.vectorize(sigmoid)
 
+def cal_grad(w, x, y, lamb=0):
+    '''
+    计算梯度
+    '''
+    L = y - (1.0 - sigmoid_vec(np.dot(w.T, x)))
+    grad = - np.dot(x, L) + lamb * w
+    return grad
+
 def gradient_descent(x, y, lamb=0):
     '''
     梯度下降法求系数矩阵 W
     '''
     w = np.zeros((x.shape[0]), dtype=np.float)
-    L = y - (1.0 - sigmoid_vec(np.dot(w.T, x)))
-    grad = - np.dot(x, L)
+    grad = cal_grad(w, x, y, lamb)
     while (np.absolute(grad) > 1e-5).all() :
         w -= grad
-        L = y - (1.0 - sigmoid_vec(np.dot(w.T, x)))
-        grad = -np.dot(x, L) + lamb * w
+        grad = cal_grad(w, x, y, lamb)
+    return w
+
+def cal_sigV(w, x):
+    V = np.zeros((x.shape[1], x.shape[1]), np.float64)
+    L = sigmoid_vec(np.dot(w.T, x))
+    for i in range(L.shape[0]):
+        V[i, i] = (1 - L[i]) * L[i]
+    return V
+
+def newton(x, y, lamb=0):
+    '''
+    牛顿法求系数矩阵 W   x.shape -> (3, 35)
+    '''
+    w = np.zeros((x.shape[0]), dtype=np.float)
+    grad = cal_grad(w, x, y, lamb)
+    V = cal_sigV(w, x)
+    H = np.dot(np.dot(x, V), x.T) + lamb
+    while (np.absolute(grad) > 1e-6).all() :
+        w -= np.dot(np.linalg.inv(H), grad)
+        grad = cal_grad(w, x, y, lamb)
+        V = cal_sigV(w, x)
+        H = np.dot(np.dot(x, V), x.T)
     return w
 
 def draw_line(w, col):
@@ -39,7 +67,15 @@ if __name__ == "__main__":
     draw_line(w, 'black')
 
     # 梯度下降法 带正则项的
-    w = gradient_descent(x.T, y, 0.0003)
+    w = gradient_descent(x.T, y, 0.003)
     draw_line(w, 'blue')
+
+    # 牛顿法 不带正则项
+    w = newton(x.T, y)
+    draw_line(w, 'red')
+
+    # 牛顿法 带正则项
+    w = newton(x.T, y, 0.003)
+    draw_line(w, 'yellow')
 
     gen.draw(x)
