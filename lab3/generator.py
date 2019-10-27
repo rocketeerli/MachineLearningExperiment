e3a78cbb70bad8f34ecaa695a71f02922b48da9a
rocketeerLi <31384630+rocketeerli@users.noreply.github.com>
2019-10-27 22:16:01 +0800

    refactoring generator

--- lab3/generator.py
+++ lab3/generator.py
@@ -1,36 +1,45 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-MEAN = np.array([[0.5, 0.5], [5.5, 5.5], [4, 1], [10, 10], [10, 5], [1, 10], [6, 10], [1, 5], [10, 1]])
-COL = ['lime', 'darkviolet', 'forestgreen', 'orange', 'yellow', 'gray', 'blue', 'r', 'black']
+'''
+生成数据
+'''
+class Generator:
+    MEAN = np.array([[0.5, 0.5], [5.5, 5.5], [4, 1], [10, 10], [10, 5], [1, 10], [6, 10], [1, 5], [10, 1]])  # 各个类的均值
+    COL = ['lime', 'darkviolet', 'forestgreen', 'orange', 'yellow', 'gray', 'blue', 'red', 'black']          # 各类的颜色
+ 
+    def __init__(self, k=3, NUM=20):
+        self.k = k                 # 生成训练数据的类别数
+        self.NUM = NUM             # 每个类别的数目  
 
-# 随机生成协方差矩阵
-def rand_cov():
-    cov = np.random.rand(2, 2)
-    cov += np.eye(*cov.shape)
-    cov[1][0] = cov[0][1]
-    return cov
+    # 高斯分布产生 k 个高斯分布的数据（不同均值和方差）
+    def data_generator(self):
+        self.x = np.random.multivariate_normal(self.MEAN[0], self.__rand_cov(), self.NUM)
+        self.y = [0 for i in range(self.NUM)]
+        for i in range(1, self.k):
+            x_i = np.random.multivariate_normal(self.MEAN[i], self.__rand_cov(), self.NUM)
+            self.x = np.vstack((self.x, x_i))
+            self.y = self.y + [i for j in range(self.NUM)]
+        self.y = np.array(self.y)
+        return self.x, self.y
 
-# 高斯分布产生 k 个高斯分布的数据（不同均值和方差）
-def data_generator(k):
-    x = np.random.multivariate_normal(MEAN[0], rand_cov(), 20)
-    y = [0 for i in range(20)]
-    for i in range(1, k):
-        x_i = np.random.multivariate_normal(MEAN[i], rand_cov(), 20)
-        x = np.vstack((x, x_i))
-        y = y + [i for j in range(20)]
-    y = np.array(y)
-    return x, y
+    # 画出 k 类样本点
+    def draw(self):
+        for i in range(self.k):
+            plt.scatter(self.x[self.NUM*i:self.NUM*(i+1),0], self.x[self.NUM*i:self.NUM*(i+1),1], c=self.COL[i])
+        plt.xlabel('x1')
+        plt.ylabel('x2')
+        plt.show()
 
-# 画出 k 类样本点
-def draw(x, k):
-    for i in range(k):
-        plt.scatter(x[20*i:20*(i+1),0], x[20*i:20*(i+1),1], c=COL[i])
-    plt.xlabel('x1')
-    plt.ylabel('x2')
-    plt.show()
+    # 随机生成协方差矩阵
+    def __rand_cov(self):
+        cov = np.random.rand(2, 2)
+        cov += np.eye(*cov.shape)
+        cov[1][0] = cov[0][1]
+        return cov
 
 if __name__ == "__main__":
     k = 9
-    x, y = data_generator(k)
-    draw(x, k)
+    generator = Generator(k)
+    x, y = generator.data_generator()
+    generator.draw()
